b6bca1b01924f26ca04c1bf010710da32a4ccf9f
acrylic-style <me@acrylicstyle.xyz>
2023-12-13 19:40:46 +0900

    update to tModLoader v2023.10.3.0

--- FlashlightConfig.cs
+++ FlashlightConfig.cs
@@ -9,13 +9,9 @@ namespace minusGN
         public override ConfigScope Mode => ConfigScope.ClientSide;
         
         [DefaultValue(true)] // easy mode
-        [Label("Attach flashlight to cursor instead of player")]
-        [Tooltip("On = Flashlight will be placed at cursor, Off = Player")]
         public bool AttachToCursor { get; set; }
         
         [DefaultValue(0)]
-        [Label("Layer")]
-        [Tooltip("0 = All UIs are visible; 19 = map is hidden; 33 = (almost) everything is hidden")]
         [Range(0, 50)]
         public int Layer { get; set; }
     }
